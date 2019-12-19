7e1f6e600c9639a2dcd7c30afcca75e94316b9f8
igor777-cool <igor_baranov@tut.by>
2019-12-19 21:45:43 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/components/Profile.module.css
    #	src/components/Profile/Post/MyPosts/MyPosts.module.css
    #	src/components/Profile/Profile.jsx
    #	src/components/Profile/Profile.module.css

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post.jsx'
-const MyPosts = () => {
-    return(
-    <div>
-        My posts
-        <div>
-            <textarea></textarea>
-            <button>Add post</button>
-        </div>
-        <div className={s.post}>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        </div>
-    </div>
-    )
-        }
 
-        export default MyPosts;
+const MyPosts = () => {
+    return (
+        <div>
+            My posts
+
+            <div>
+                <textarea></textarea>
+                <button>Add post</button>
+            </div>
+            <div className={s.posts}>
+                <Post message='Hi! How are you?' counter='25'/>
+                <Post message="It's my first post" counter='21'/>
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default MyPosts;
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
-    return (
+const Post = (props) => {
 
+    return (
         <div className={s.item}>
-            <img  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbUHn7N3gmyjjSlcEIC2UmlTfkSIvNr7GN6C8CGEpo7_pw_uQ1&s'/>
-            post !!!
+            <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbUHn7N3gmyjjSlcEIC2UmlTfkSIvNr7GN6C8CGEpo7_pw_uQ1&s'/>
+            {props.message}
+
             <div>
-            <button>LIKE</button>
+
+                <button>LIKE</button>
+                <button>{props.counter}</button>
+
+
+
             </div>
         </div>
-
 
 
     )
